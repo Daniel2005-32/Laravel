bfa41b681595948cc897b8e907c955b2414a4837
Daniel2005-32 <danielsmartin2005@gmail.com>
2026-02-02 18:42:40 +0000

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LibroController;
-use App\Http\Controllers\UsuarioController;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-Route::middleware(['auth'])->group(function () {
-    // Admin Dashboard
-    Route::get('/admin', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
-
-    // Rutas para Libros
-    Route::get('/libros', [LibroController::class, 'index'])->name('libro.index');
-    Route::match(['get', 'post'], '/libros/crear', [LibroController::class, 'create'])->name('libro.create');
-    Route::get('/libros/mostrar/{id}', [LibroController::class, 'show'])->name('libro.show');
-    Route::match(['get', 'post'], '/libros/editar/{id?}', [LibroController::class, 'edit'])->name('libro.edit');
-    Route::match(['get', 'post'], '/libros/borrar/{id?}', [LibroController::class, 'destroy'])->name('libro.destroy');
-
-    // Rutas para Usuarios
-    Route::get('/usuarios/mostrar/{id}', [UsuarioController::class, 'show'])->name('usuario.show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
